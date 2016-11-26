e0838f895b9d11c1419e601e1e4a87aa9fd29e7d
thexa4 <thexa4@gmail.com>
2016-11-26 18:55:38 +0000

    Previous uncommitted changes

--- mjs5/js/mjs5.js
+++ mjs5/js/mjs5.js
@@ -2,8 +2,6 @@ $(function(){
     OAuth2 = new OAuth2Handler("mjs5", "http://musicbrainz.i.bolkhuis.nl/mjs5/");
     OAuth2.login("mp3control", function(){
         window.SongController = new SongController();
-        SongController.get("http://musicbrainz.i.bolkhuis.nl//plugin/file/files/browse/Artists/ACDC/Black Ice/0101 - Rock 'n' Roll Train.mp3", function(result) {
-            console.log(result);
-        });
-    }, function(result) {console.log(result); });
+        window.mjs = new MjsController("mp3soos", OAuth2);
+    }, function(result) { console.log("error"); console.log(result); });
 });
